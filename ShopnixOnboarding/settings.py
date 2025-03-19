ee749b71dfcb6e71936eed32cac42d8f933fd280
Goutham D <your-email@example.com>
2025-03-20 00:32:49 +0530

    HI

--- ShopnixOnboarding/settings.py
+++ ShopnixOnboarding/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,6 +129,6 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"  # For production
 
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # settings.py
 
--- onboarding/urls.py
+++ onboarding/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),  # Homepage
-    path('verify-otp/', views.verify_otp, name='verify_otp'),  # Verify OTP
-    path('create-store/', views.create_store, name='create_store'),  # Create Store
+    path('verify_otp/', views.verify_otp, name='verify_otp'),  # Verify OTP
+    path('create_store/', views.create_store, name='create_store'),  # Create Store
     path('step1/', views.step1, name='step1'),  # Step 1 - Theme Selection
-    path('apply-theme/', views.apply_theme, name='apply_theme'),  # Apply Theme
+    path('apply_theme/', views.apply_theme, name='apply_theme'),  # Apply Theme
     path('step2/', views.step2, name='step2'),  # Step 2 - Add Product Type and Category
     path('add_product/', views.add_product, name='add_product'),
-    path('setup-store/', views.setup_store, name='setup_store'),
+    path('setup_store/', views.setup_store, name='setup_store'),
     path('step3/', views.step3, name='step3'),
 
 ]
